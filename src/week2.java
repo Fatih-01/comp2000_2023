8b5ddfe5daa562a75fc56621c61edf09cc5d290b
Fatih-01 <fatihazbentt@gmail.com>
2023-07-31 12:15:35 +1000

    test for adding into week02 repo to my main repo

---
+++ src/week2.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+public class week2 {
+    //WEEK 2 LECTURE NOTES
+    static Dimension d1 = new Dimension(1024, 780);
+    
+    public static void main(String[] args){
+        System.out.println(d1);
+
+        Dimension d2 = new Dimension();
+        System.out.println(d2);
+    }
+}
